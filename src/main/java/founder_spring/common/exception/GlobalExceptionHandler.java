fd29fab484bf48b0601ba214558f8d595a87b6d2
khNg2302 <ngkh161@gmail.com>
2026-08-30 13:08:52 +0700

    git commit -m "feat: standardize invalid request error response"

--- src/main/java/founder_spring/common/exception/GlobalExceptionHandler.java
+++ src/main/java/founder_spring/common/exception/GlobalExceptionHandler.java
@@ -4,13 +4,52 @@ import founder_spring.category.exception.CategoryNotFoundException;
 import founder_spring.category.exception.InvalidCategoryException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import tools.jackson.databind.exc.InvalidFormatException;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex
+    ) {
+        String message = "Invalid request body";
+
+        Throwable cause = ex.getCause();
+
+        if (cause instanceof InvalidFormatException invalidFormatException
+                && invalidFormatException.getTargetType().isEnum()) {
+
+            Class<?> enumClass = invalidFormatException.getTargetType();
+
+            Object[] values = enumClass.getEnumConstants();
+
+            String allowedValues = Arrays.stream(values)
+                    .map(Object::toString)
+                    .collect(Collectors.joining(", "));
+
+            message = "Invalid value '"
+                    + invalidFormatException.getValue()
+                    + "'. Allowed values: "
+                    + allowedValues;
+        }
+
+        return new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "INVALID_REQUEST_BODY",
+                message,
+                LocalDateTime.now()
+        );
+    }
 
     @ExceptionHandler(CategoryNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleCategoryNotFound(
